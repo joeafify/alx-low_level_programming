7a4e5531ae56c55f182431d72c6f3e2eb765ad0c
Youssef Afify <joe.afify95@gmail.com>
2023-02-17 07:04:03 -0800

    Re: 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
