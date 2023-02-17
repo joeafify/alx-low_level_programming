1996db6504b2f8d29487ed1fa286921318445166
Youssef Afify <joe.afify95@gmail.com>
2023-02-17 13:35:32 -0800

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Display the state of a number (positive or negative)'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+        putchar(c);     
+	}
+	putchar('\n');
+	return (0);
+}
