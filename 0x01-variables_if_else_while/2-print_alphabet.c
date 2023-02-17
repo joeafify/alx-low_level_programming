4a969ed789266e2113b1888b8c022af62993e89d
Youssef Afify <joe.afify95@gmail.com>
2023-02-17 13:30:31 -0800

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Display the state of a number (positive or negative)'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
+}
