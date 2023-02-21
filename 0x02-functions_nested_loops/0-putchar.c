21a9bb17567c18229c9e8101c89f4617707caca6
Youssef Afify <joe.afify95@gmail.com>
2023-02-21 12:36:03 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Display the state of a number (positive or negative)'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	putchar('\n');
+	return (0);
+}
